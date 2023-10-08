c7dbcc642cade6a8ab6862d2d78f3483b69100bf
ahmedgado0o <lucian_2010@hotmail.com>
2023-10-08 11:46:15 -0700

    lists

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x = tuple_a[0] if len(tuple_a) > 0 else 0
+    y = tuple_a[1] if len(tuple_a) > 1 else 0
+    z = tuple_b[0] if len(tuple_b) > 0 else 0
+    k = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (x + z, y + k)
