3b71933e79837b839350092f2b2759890f3a6c1e
ahmedgado0o <lucian_2010@hotmail.com>
2023-10-23 11:38:12 -0700

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i, j = 0, 0
+
     while i < x:
         try:
             print("{:d}".fotmat(my_list[i]), end='')
-            c += 1
+            j += 1
         except (ValueError, TypeError):
             pass
         i += 1
     print()
-    return c
+    return j
