dab53a4c2f9a73ee46b9660ce279a5c62431ea65
ahmedgado0o <lucian_2010@hotmail.com>
2023-10-24 12:47:11 -0700

    classis

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,17 @@
 """Square class"""
 
 
-def __init__(self, size):
-    """Constructor.
+class Square:
+    """Square"""
 
-    Args:
-        size: size
-    """
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
-    seld.__size = size
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: size
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        seld.__size = size
