5a4e603c572e348853f09cfbec480e8686e27c64
ahmedgado0o <lucian_2010@hotmail.com>
2023-11-07 12:24:39 -0800

    imput output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     '''creats object'''
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.lead(f)
+        return json.load(f)
